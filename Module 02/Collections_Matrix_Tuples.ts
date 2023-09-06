1b260230a37370408c4b54e1c9cc3906744964b6
Nayrb29 <bryan201429@gmail.com>
2023-09-05 22:59:51 -0500

    Labs

--- Module 02/Collections_Matrix_Tuples.ts
+++ Module 02/Collections_Matrix_Tuples.ts
@@ -5,4 +5,7 @@ let list2: Array<number> = [1,2,3];
 
 //?Tuples: 
 let person1:[string,number]=['Maria',35];
-let person2:
+// let person2:[string,number]=['Maria',30,true] //!Error, solo contiene esos tipos
+// let person3:[string,number]=[35,'Maria']; //! Error, los datos estan invertidos
+
+
