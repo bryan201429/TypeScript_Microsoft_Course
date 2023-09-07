51ce3a5649e1bbc3ec2e3f077eb995c8d75afa8b
Nayrb29 <bryan201429@gmail.com>
2023-09-06 22:41:23 -0500

    M03

--- Module 03 Interfaces/Interface declaration.ts
+++ Module 03 Interfaces/Interface declaration.ts
@@ -3,9 +3,20 @@ interface IceCream{
     scoops: number;
 }
 
+function tooManyScoops(desert:IceCream){
+    if(desert.scoops>=4){
+        return desert.scoops + ' is too many scoops!';
+    } else{
+        return 'Your order will be ready';
+    }
+}
+
 let myIceCream:IceCream={
     flavor:'vanilla',
     scoops:2
 }
 
-console.log(myIceCream)
+// console.log(myIceCream)
+
+
+console.log(tooManyScoops({flavor:'chocolate',scoops:4}))
